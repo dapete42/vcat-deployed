077d93350770c141d1189d74f6b0712a79251f0c
Peter Schlömer (dapete) <dev@dapete.net>
2013-10-03 11:23:34 +0000

    Reduce Java memory allocation in start script

--- work/cron.sh
+++ work/cron.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-jstart -stderr -mem 2g -o /data/project/vcat/vcat-toollabs-daemon.log -j y -N vcat-toollabs-daemon java -Xms256m -Xmx256m -jar $HOME/work/jar/vcat-toollabs.jar $HOME/work/vcat.properties
+jstart -stderr -mem 2g -o /data/project/vcat/vcat-toollabs-daemon.log -j y -N vcat-toollabs-daemon java -Xms128m -Xmx128m -jar $HOME/work/jar/vcat-toollabs.jar $HOME/work/vcat.properties
