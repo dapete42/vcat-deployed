be40aa98052e6aaa67c5ec329772a8414f6cbc29
Peter Schlömer (dapete) <dev@dapete.net>
2013-11-22 21:57:10 +0000

    Improved start scripts; cron.sh checks if the daemon is actually listening, not just if the SGE job is still there

--- bin/cron.sh
+++ bin/cron.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-$HOME/bin/start-job >/dev/null 2>&1
+$HOME/bin/vcat-control-ping
+NOT_LISTENING="$?"
+
+if [ $NOT_LISTENING -eq 1 ]; then
+	$HOME/bin/restart-job >/dev/null 2>&1
+else
+	$HOME/bin/start-job >/dev/null 2>&1
+fi
