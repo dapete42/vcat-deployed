98d4f838aec08af889ed0e43a91c6826c0226bfd
Peter Schlömer (dapete) <dev@dapete.net>
2013-11-23 22:39:13 +0000

    Fixes to control scripts

--- bin/cron.sh
+++ bin/cron.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-$HOME/bin/vcat-control-ping
-NOT_LISTENING="$?"
+# Check if SGE job is running
+JOBID=`job vcat-toollabs-daemon`
 
-if [ $NOT_LISTENING -eq 1 ]; then
-	$HOME/bin/restart-job >/dev/null 2>&1
-else
+if [ -z "$JOBID" ]; then
+	# If job is not running, start it
 	$HOME/bin/start-job >/dev/null 2>&1
+else
+	# If job is running, check if it is still listening
+	$HOME/bin/vcat-control-ping
+	NOT_LISTENING="$?"
+	if [ $NOT_LISTENING -eq 1 ]; then
+		# If not listening, restart
+		$HOME/bin/restart-job >/dev/null 2>&1
+	fi
+
 fi
