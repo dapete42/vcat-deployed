2dfdf22d3161316de9e25d35631c3765adc964f2
Peter Schlömer (dapete) <dev@dapete.net>
2014-09-03 15:22:30 +0000

    webservice can now be started from cronjobs

--- bin/cron.sh
+++ bin/cron.sh
@@ -5,7 +5,6 @@ JOBID=`job tomcat-vcat`
 
 if [ -z "$JOBID" ]; then
         # If job is not running, start it
-        #webservice -tomcat start >/dev/null 2>&1
-	qsub -e $HOME/error.log -o /dev/null -i /dev/null -q "webgrid-tomcat" -l h_vmem=4g -b y -N "tomcat-vcat" /usr/local/bin/tool-tomcat >/dev/null 2>&1
+	webservice -tomcat start >/dev/null 2>&1
 fi
 
